1dd38128dad76732ca86148d209b702fb829f5c8
Zane Hillenburg <hillenburgzane@gmail.com>
2023-07-06 13:40:39 -0700

    fsars

--- jav.js
+++ jav.js
@@ -1,1 +1,25 @@
-le
+let firstpg=document.querySelector(".firstpg");
+
+
+    function bou(){
+        window.location.href="about-me.html"
+    }
+function hom(){
+    window.location.href="index.html"
+    }
+function exp(){
+    window.location.href="experience.html"
+    }
+function exploit(){
+    window.location.href="exploitpg.html"
+    }
+function contact(){
+    window.location.href="contact.html"
+    }
+
+
+
+
+
+
+
